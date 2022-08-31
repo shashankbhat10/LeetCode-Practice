7b3b94dbdf8fdb0652782ee0a02195cd36bb5f93
Shashank Bhat <s.2205.b@gmail.com>
2022-08-31 13:53:54 -0400

    Time: 4 ms (22.64%), Space: 42.1 MB (7.34%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,16 +1,35 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
+//         int slow = n;
+//         int fast = n;
         
-        do{
-            slow = digitSum(slow);
-            fast = digitSum(digitSum(fast));
-            if(slow == 1)
-                return true;
-        }while(slow != fast);
+//         do{
+//             slow = digitSum(slow);
+//             fast = digitSum(digitSum(fast));
+//             if(slow == 1)
+//                 return true;
+//         }while(slow != fast);
         
-        return false;
+//         return false;
+        Set<Integer> set = new HashSet<Integer>();
+        int temp = n;
+        while(temp != 1){
+            if(set.contains(temp))
+                return false;
+            set.add(temp);
+            temp = getSum(temp);
+            // System.out.println(temp);
+        }
+        return true;
+    }
+    
+    public int getSum(int n){
+        int sum = 0;
+        while(n != 0){
+            sum += Math.pow(n % 10, 2);
+            n = n / 10;
+        }
+        return sum;
     }
     
     private int digitSum(int n){
