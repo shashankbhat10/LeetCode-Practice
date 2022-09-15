6970a7914bc9f750fb1eaf5e4d23717fd40fa178
Shashank Bhat <s.2205.b@gmail.com>
2022-09-15 17:38:47 -0400

    Time: 28 ms (19.23%), Space: 80 MB (22.51%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -9,29 +9,74 @@
  * }
  */
 class Solution {
+    public ListNode merge(ListNode h1, ListNode h2){
+        if(h1 == null){
+            return h2;
+        }
+        if(h2 == null){
+            return h1;
+        }
+        
+        if(h1.val < h2.val){
+            h1.next = merge(h1.next, h2);
+            return h1;
+        }
+        else{
+            h2.next = merge(h1, h2.next);
+            return h2;
+        }
+        
+    }
+    
     public ListNode sortList(ListNode head) {
-        if(head == null)
-            return null;
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+//         if(head == null)
+//             return null;
+//         PriorityQueue<Integer> q = new PriorityQueue<Integer>();
         
-        ListNode curr = head;
+//         ListNode curr = head;
         
-        while(curr != null){
-            q.add(curr.val);
-            curr = curr.next;
+//         while(curr != null){
+//             q.add(curr.val);
+//             curr = curr.next;
+//         }
+        
+//         ListNode node = new ListNode(q.peek());
+//         node.next = null;
+//         q.remove();
+//         curr = node;
+//         while(!q.isEmpty()){
+//             ListNode temp = new ListNode(q.remove());
+//             temp.next = null;
+//             curr.next = temp;
+//             curr = curr.next;
+//         }
+        
+//         return node;
+        
+        if(head == null){
+            return head;
+        }
+        if(head.next == null){
+            return head;
         }
         
-        ListNode node = new ListNode(q.peek());
-        node.next = null;
-        q.remove();
-        curr = node;
-        while(!q.isEmpty()){
-            ListNode temp = new ListNode(q.remove());
-            temp.next = null;
-            curr.next = temp;
-            curr = curr.next;
-        }
+        //p1 move 1 step every time, p2 move 2 step every time, pre record node before p1
+        ListNode p1 = head;
+        ListNode p2 = head;
+        ListNode pre = head;
         
-        return node;
+        while(p2 != null && p2.next != null){
+            pre = p1;
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        //change pre next to null, make two sub list(head to pre, p1 to p2)
+        pre.next = null;
+        
+        //handle those two sub list
+        ListNode h1 = sortList(head);
+        ListNode h2 = sortList(p1);
+        
+        return merge(h1, h2);
     }
 }
