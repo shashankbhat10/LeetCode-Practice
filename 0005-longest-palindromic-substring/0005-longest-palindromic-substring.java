7fc3b8c31bea194313ba7d29f73a30b3f0f0c832
Shashank Bhat <s.2205.b@gmail.com>
2022-10-27 18:57:49 -0400

    Time: 406 ms (17.30%), Space: 121.8 MB (8.89%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    Boolean[][] memo ;
+    
+    public String longestPalindrome(String s) {
+        
+        int len = s.length();
+        
+        int maxLen = 0;
+        String res = "";
+        memo = new Boolean[len][len];
+        
+        for (int left = 0; left < len; left++){
+            for (int right = left; right < len; right++){
+                
+             if (s.charAt(left) == s.charAt(right) && isPalin(s,left + 1, right -1)){
+                if (maxLen < right - left + 1) {
+                    maxLen = right - left + 1;
+                    res = s.substring(left, right + 1);
+                    }
+                }   
+            }
+        }
+        
+        return res;
+    }
+    
+    
+    public boolean isPalin(String s, int left, int right){
+        
+        if (left >= right){
+            return true;
+        }
+        
+        if (memo[left][right] != null){
+            return memo[left][right] ;
+        }
+        
+        if (s.charAt(left) != s.charAt(right)) {
+            memo[left][right] = false;
+            return memo[left][right];
+        }
+        
+        if (right - left <= 2){
+            memo[left][right] = true;
+            return memo[left][right];
+        }
+        
+        memo[left][right] = isPalin(s,left + 1, right - 1);
+        return memo[left][right];
+        
+    }
+}
