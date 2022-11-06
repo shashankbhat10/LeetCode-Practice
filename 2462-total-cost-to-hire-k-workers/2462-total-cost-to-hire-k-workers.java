eff329f369934b4b7017e866d9a2a439b736e09a
Shashank Bhat <s.2205.b@gmail.com>
2022-11-06 00:14:15 -0400

    Time: 87 ms (100.00%), Space: 79 MB (20.00%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -48,37 +48,23 @@ class Solution {
                 int rightMin = rightQueue.peek();
                 if(leftMin <= rightMin){
                     min = leftMin;
-                    // System.out.println("before: " + min);
                     int temp = leftQueue.remove();
-                    // System.out.println("after: " + leftQueue.peek());
                 } else {
                     min = rightMin;
-                    // System.out.println("before: " + min);
                     int temp = rightQueue.remove();
-                    // System.out.println("after: " + rightQueue.peek());
                 }
                 
-                // System.out.println("here");
-                // System.out.println("fixed: " + candidates*2);
-                // System.out.println("size: " + (leftQueue.size() + rightQueue.size()));
                 if(candidates*2-1 <= leftQueue.size() + rightQueue.size()){
-                    // System.out.println("qweq");
-                    // System.out.println("min: " + min);
-                    // System.out.println("leftMin: " + leftMin);
-                    // System.out.println("rightMin: " + rightMin);
                     if(min == leftMin){
-                        // System.out.println("Here: " + costs[leftIndex]);
                         leftQueue.add(costs[leftIndex]);
                         leftIndex++;
                     } else {
-                        // System.out.println("rightIndex: " + rightIndex);
                         rightQueue.add(costs[rightIndex]);
                         rightIndex--;
                     }
                 }
             }
             
-            // System.out.println(min);
             totalCost += (long) min;
         }
         return totalCost;
